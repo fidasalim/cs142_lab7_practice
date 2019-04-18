9b73299a2d4e3912e91c4e56c0fbb3eff1184055
fidasalim <42141530+fidasalim@users.noreply.github.com>
2019-04-18 12:49:44 +0530

    Add files via upload

---
+++ heap.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+using namespace std;
+//class for heap
+class BinaryHeap(){
+	//pointer to the array
+	int *hArr;
+	//maximum size
+	int mSize;
+	//current size
+	int cSize;
+	
+	//**************************************************************************
+	
+	//constructer
+	Binaryheap(int mSize){
+		hArr = new int[mSize];
+		this->mSize = mSize;
+		cSize = 0;
+	}
+	//**************************************************************************
+	void swap(int *a,int *b){
+		int temp = *a;
+		*a = *b;
+		*b = temp;
+	}
+	//**************************************************************************
+	int left(int i){ return (i*2)+1;}
+	int right(int i){ return (i*2)+2;}
+	int parent(int i){ return (i-1)/2;}
+	
+	int get_min(){return hArr[0];}
+	//**************************************************************************
+	void insert(int value){
+		//when overflowed
+		if(cSize == mSize){
+			cout<<"noT possible"<<endl;
+			return;
+		}
+		else{
+			//inserting new value;
+			hArr[cSize] = value;
+			//ensuring the rule is carried over
+			int i = cSize;
+			while(i!=0 && hArr[parent(i)]>hArr[i]){
+				swap(&hArr[parent(i)],&hArr[i]);
+				i = parent(i);
+			}
+			//incresing the cSize
+			cSize++;
+		}
+	}
+	
+	//**************************************************************************
+	void display(){
+		for(int i = 0; i < cSize; i++){
+			cout<<hArr[i]<<",";
+		}
+		cout<<endl;
+	}
+	
+	//**************************************************************************
+	void MinHeapify(int i){
+		int smallest;
+		if(left(i),right(i)<cSize){
+			if(hArr[left(i)]<hArr(i)){
+				smallest = left(i);
+			}
+			if(hArr[right(i)]<hArr[smallest]){
+				smallest = right(i);
+			}
+			if(smallest!=i){
+				swap(&hArr[smallest],&hArr[i]);
+			}
+			MinHeapify(smallest);
+		}
+		else return;
+	}
+	
+	
+	//**************************************************************************
+	void ExtraxtMin(int index){
+		if(cSize == 0){
+			cout<<"No sufficient elements"<<endl;
+		}
+		else if(cSize == 1){
+			cSize = 0;
+		}
+		else{
+			swap(&hArr[i],&hArr[cSize-1]);
+			MinHeapify(i);
+			return hArr[index];
+		}
+	}
+	//**************************************************************************
+	
+	
+	//**************************************************************************
+	
+};
+
